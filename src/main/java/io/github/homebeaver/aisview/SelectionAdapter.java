fc84cfa57d089d1b4aea3f30efa5fcf92ff151d6
homebeaver <homebeaver@outlook.de>
2025-10-23 17:49:24 +0200

    Use java Rectangle.contains instead jts package
    	die Umrechnerei in GeoPosition kann man sich ersparen
    	- mit convertGeoPositionToPoint GeoPosition in Point2D umwandeln p
    	- und mit java Mitteln
    	Rectangle.contains(p) berechnen

--- src/main/java/io/github/homebeaver/aisview/SelectionAdapter.java
+++ src/main/java/io/github/homebeaver/aisview/SelectionAdapter.java
@@ -1,88 +1,101 @@
-/* created from jxmapviewer sample3_interaction
-*/ 
 package io.github.homebeaver.aisview;
 
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
-import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.viewer.Waypoint;
 
 /**
  * Creates a selection rectangle based on mouse input
- * Also triggers repaint events in the viewer
- * @author Martin Steiger
+ * <p>
+ * Also triggers repaint events in the viewer (draws the selection rectangle)
+ * and sets the candidatesToTrack List property (vessels inside the selection rectangle)
+ * 
  */
-public class SelectionAdapter extends MouseAdapter 
-{
-    private boolean dragging;
-    private JXMapViewer viewer;
+public class SelectionAdapter extends MouseAdapter {
 
-    private Point2D startPos = new Point2D.Double();
-    private Point2D endPos = new Point2D.Double();
+	private static final Logger LOG = Logger.getLogger(SelectionAdapter.class.getName());
 
-    /**
-     * @param viewer the jxmapviewer
-     */
-    public SelectionAdapter(JXMapViewer viewer)
-    {
-        this.viewer = viewer;
-    }
+	private boolean dragging;
+	private AisMapKit viewer;
 
-    @Override
-    public void mousePressed(MouseEvent e)
-    {
-        if (e.getButton() != MouseEvent.BUTTON3)
-            return;
+	private Point2D startPos = new Point2D.Double();
+	private Point2D endPos = new Point2D.Double();
 
-        startPos.setLocation(e.getX(), e.getY());
-        endPos.setLocation(e.getX(), e.getY());
+	/**
+	 * @param viewer the jxmapviewer
+	 */
+	public SelectionAdapter(AisMapKit viewer) {
+		this.viewer = viewer;
+	}
 
-        dragging = true;
-    }
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() != MouseEvent.BUTTON3) return;
 
-    @Override
-    public void mouseDragged(MouseEvent e)
-    {
-        if (!dragging)
-            return;
+		startPos.setLocation(e.getX(), e.getY());
+		endPos.setLocation(e.getX(), e.getY());
 
-        endPos.setLocation(e.getX(), e.getY());
+		dragging = true;
+	}
 
-        viewer.repaint();
-    }
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (!dragging) return;
 
-    @Override
-    public void mouseReleased(MouseEvent e)
-    {
-        if (!dragging)
-            return;
+		endPos.setLocation(e.getX(), e.getY());
 
-        if (e.getButton() != MouseEvent.BUTTON3)
-            return;
+		viewer.repaint(); // draws the selection rectangle
+	}
 
-        viewer.repaint();
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if (!dragging) return;
+		if (e.getButton() != MouseEvent.BUTTON3) return;
 
-        dragging = false;
-    }
+		viewer.repaint(); // draws the selection rectangle
+		
+		// welche Schiffe sind ausgewählt?
+		viewer.setCandidatesToTrack(findVesselsInSelectedRectangle());
 
-    /**
-     * @return the selection rectangle
-     */
-    public Rectangle getRectangle()
-    {
-        if (dragging)
-        {
-            int x1 = (int) Math.min(startPos.getX(), endPos.getX());
-            int y1 = (int) Math.min(startPos.getY(), endPos.getY());
-            int x2 = (int) Math.max(startPos.getX(), endPos.getX());
-            int y2 = (int) Math.max(startPos.getY(), endPos.getY());
+		dragging = false; // end dragging
 
-            return new Rectangle(x1, y1, x2-x1, y2-y1);
-        }
+	}
 
-        return null;
-    }
+	private List<Integer> findVesselsInSelectedRectangle() {
+		Waypoint[] w = new Waypoint[1];
+		List<Integer> mmsiList = new ArrayList<Integer>();
+		viewer.locationPainters.forEach( (k,v) -> {
+			assert v.getWaypoints().size()==1;
+			v.getWaypoints().toArray(w);
+			Point2D p = viewer.getMainMap().convertGeoPositionToPoint(w[0].getPosition());
+			if (getRectangle().contains(p)) {
+				LOG.fine("ausgewählt ist "+k);
+				mmsiList.add(k);
+			}
+		});
+		return mmsiList;
+	}
+
+	/**
+	 * @return the selection rectangle
+	 */
+	public Rectangle getRectangle() {
+		if (dragging) {
+			int x1 = (int) Math.min(startPos.getX(), endPos.getX());
+			int y1 = (int) Math.min(startPos.getY(), endPos.getY());
+			int x2 = (int) Math.max(startPos.getX(), endPos.getX());
+			int y2 = (int) Math.max(startPos.getY(), endPos.getY());
+
+			return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+		}
+
+		return null;
+	}
 
 }
--- src/main/java/io/github/homebeaver/aisview/SelectionPainter.java
+++ src/main/java/io/github/homebeaver/aisview/SelectionPainter.java
@@ -1,26 +1,17 @@
-/* created from jxmapviewer sample3_interaction
-*/ 
 package io.github.homebeaver.aisview;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.geom.Point2D;
-import java.util.logging.Logger;
 
 import javax.swing.Painter;
 
-import org.jxmapviewer.viewer.GeoPosition;
-import org.jxmapviewer.viewer.TileFactory;
-
 /**
  * Paints a selection rectangle
- * @author Martin Steiger
  */
 public class SelectionPainter<T> implements Painter<T> {
 
-	private static final Logger LOG = Logger.getLogger(SelectionPainter.class.getName());
+//	private static final Logger LOG = Logger.getLogger(SelectionPainter.class.getName());
 
 	private Color fillColor = new Color(128, 192, 255, 128);
 	private Color frameColor = new Color(0, 0, 255, 128);
@@ -43,45 +34,6 @@ public class SelectionPainter<T> implements Painter<T> {
 			g.draw(rc);
 			g.setColor(fillColor);
 			g.fill(rc);
-
-/*
-            Rectangle bounds = viewer.getViewportBounds();
-            int x = bounds.x + evt.getX();
-            int y = bounds.y + evt.getY();
-            Point pixelCoordinates = new Point(x, y);
-            mapClicked(viewer.getTileFactory().pixelToGeo(pixelCoordinates, viewer.getZoom()));
-
- * /
-			Rectangle bounds = adapter.getViewer().getViewportBounds();
-			double x = adapter.getStartPos().getX() + bounds.x;
-			double y = adapter.getStartPos().getY() + bounds.y;
-			Point2D startPoint = new Point2D.Double(x, y);
-			Point2D endPoint = new Point2D.Double(adapter.getEndPos().getX() + bounds.x, adapter.getEndPos().getY() + bounds.y);
-
-			//new GeoPosition(55.70, 12.54)); // "København - Øresund"
-			TileFactory tf = adapter.getViewer().getTileFactory();
-			Point2D topLeft = new Point2D.Double(rc.getX(), rc.getY());
-			GeoPosition tl = tf.pixelToGeo(topLeft, adapter.getViewer().getZoom());
-			GeoPosition sp = tf.pixelToGeo(startPoint, adapter.getViewer().getZoom());
-			GeoPosition ep = tf.pixelToGeo(endPoint, adapter.getViewer().getZoom());
-			// einfacher:
-			GeoPosition sp2 = adapter.getViewer().convertPointToGeoPosition(adapter.getStartPos());
-			GeoPosition ep2 = adapter.getViewer().convertPointToGeoPosition(adapter.getEndPos());
-			LOG.info("dregging Rectangle:"+rc + " ViewportBounds:"+adapter.getViewer().getViewportBounds()
-				+ "\n Top-Left="+rc.getBounds2D().getX()+","+rc.getBounds2D().getY() + " == " +topLeft + " ~ "+sp + "=="+sp2
-				+ "\n Top-Right="+rc.getBounds2D().getMaxX()+","+rc.getBounds2D().getMinY()
-				+ "\n Bottom-Left="+rc.getBounds2D().getMinX()+","+rc.getBounds2D().getMaxY()
-				+ "\n Bottom-Right="+rc.getBounds2D().getMaxX()+","+rc.getBounds2D().getMaxY() + " ~ "+ep
-				);
-			//rc.contains(Point2D p) ==> 
-/ *
-	die Umrechnerei in GeoPosition kann man sich ersparen 
-	- mit convertGeoPositionToPoint GeoPosition in Point2D umwandeln p
-	- und mit java Mitteln
-	Rectangle.contains(p) berechnen
-
-
- */
 		}
 	}
 }
