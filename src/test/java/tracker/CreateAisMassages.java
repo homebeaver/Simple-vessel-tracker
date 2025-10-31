559938587a689c8e7fe28779d2e4806fa1e05272
homebeaver <homebeaver@outlook.de>
2025-10-31 17:06:57 +0100

    Class to create AIS Stream messages

---
+++ src/test/java/tracker/CreateAisMassages.java
@@ -0,0 +1,24 @@
+package tracker;
+
+import java.net.URISyntaxException;
+
+import io.github.homebeaver.aismodel.AisStreamKeyProvider;
+import io.github.homebeaver.aismodel.AisStreamWebsocketClient;
+import io.github.homebeaver.aisview.Regions;
+
+/**
+ * Class to create AIS Stream messages. The messages are printed to System.out.
+ */
+public class CreateAisMassages {
+
+	public static void main(String[] args) throws URISyntaxException {
+//		System.out.println("starting");
+		
+		AisStreamKeyProvider.getInstance().run();
+		AisStreamWebsocketClient client = 
+			new AisStreamWebsocketClient(true, AisStreamKeyProvider.getInstance(),
+					Regions.getInstance().getBoundingBox("Global"));
+		client.connect();
+	}
+
+}
