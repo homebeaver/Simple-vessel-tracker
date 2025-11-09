d6462838b30c9b7c9069edfcb7d28c72b5384aff
homebeaver <homebeaver@outlook.de>
2025-11-09 11:36:53 +0100

    CoordinatedUTCInquiry Message 10: UTC and date inquiry

--- src/main/java/io/github/homebeaver/aismodel/CoordinatedUTCInquiry.java
+++ src/main/java/io/github/homebeaver/aismodel/CoordinatedUTCInquiry.java
@@ -13,60 +13,37 @@
 
 package io.github.homebeaver.aismodel;
 
-import java.util.Objects;
-import java.util.Arrays;
-import com.google.gson.TypeAdapter;
-import com.google.gson.annotations.JsonAdapter;
-import com.google.gson.annotations.SerializedName;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import java.io.IOException;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.TypeAdapterFactory;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 //import org.openapitools.client.JSON;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import io.swagger.annotations.ApiModelProperty;
+
 /**
- * CoordinatedUTCInquiry
+ * CoordinatedUTCInquiry Message 10: UTC and date inquiry
+ * <p>
+ * This message should be used when a station is requesting UTC and date from another station.
  */
+// station == Schiff
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-06-27T20:57:25.293422-07:00[America/Vancouver]")
 public class CoordinatedUTCInquiry extends AisMessage {
-//  public static final String SERIALIZED_NAME_MESSAGE_I_D = "MessageID";
-//  @SerializedName(SERIALIZED_NAME_MESSAGE_I_D)
-//  private Integer messageID;
-//
-//  public static final String SERIALIZED_NAME_REPEAT_INDICATOR = "RepeatIndicator";
-//  @SerializedName(SERIALIZED_NAME_REPEAT_INDICATOR)
-//  private Integer repeatIndicator;
-//
-//  public static final String SERIALIZED_NAME_USER_I_D = "UserID";
-//  @SerializedName(SERIALIZED_NAME_USER_I_D)
-//  private Integer userID;
-//
-//  public static final String SERIALIZED_NAME_VALID = "Valid";
-//  @SerializedName(SERIALIZED_NAME_VALID)
-//  private Boolean valid;
-//
+
   public static final String SERIALIZED_NAME_SPARE1 = "Spare1";
   @SerializedName(SERIALIZED_NAME_SPARE1)
   private Integer spare1;
@@ -83,100 +60,7 @@ public class CoordinatedUTCInquiry extends AisMessage {
 		super();
 	}
 
-//  public CoordinatedUTCInquiry messageID(Integer messageID) {
-//    
-//    this.messageID = messageID;
-//    return this;
-//  }
-//
-//   /**
-//   * Get messageID
-//   * @return messageID
-//  **/
-//  @javax.annotation.Nonnull
-//  @ApiModelProperty(required = true, value = "")
-//
-//  public Integer getMessageID() {
-//    return messageID;
-//  }
-//
-//
-//  public void setMessageID(Integer messageID) {
-//    this.messageID = messageID;
-//  }
-//
-//
-//  public CoordinatedUTCInquiry repeatIndicator(Integer repeatIndicator) {
-//    
-//    this.repeatIndicator = repeatIndicator;
-//    return this;
-//  }
-//
-//   /**
-//   * Get repeatIndicator
-//   * @return repeatIndicator
-//  **/
-//  @javax.annotation.Nonnull
-//  @ApiModelProperty(required = true, value = "")
-//
-//  public Integer getRepeatIndicator() {
-//    return repeatIndicator;
-//  }
-//
-//
-//  public void setRepeatIndicator(Integer repeatIndicator) {
-//    this.repeatIndicator = repeatIndicator;
-//  }
-//
-//
-//  public CoordinatedUTCInquiry userID(Integer userID) {
-//    
-//    this.userID = userID;
-//    return this;
-//  }
-//
-//   /**
-//   * Get userID
-//   * @return userID
-//  **/
-//  @javax.annotation.Nonnull
-//  @ApiModelProperty(required = true, value = "")
-//
-//  public Integer getUserID() {
-//    return userID;
-//  }
-//
-//
-//  public void setUserID(Integer userID) {
-//    this.userID = userID;
-//  }
-//
-//
-//  public CoordinatedUTCInquiry valid(Boolean valid) {
-//    
-//    this.valid = valid;
-//    return this;
-//  }
-//
-//   /**
-//   * Get valid
-//   * @return valid
-//  **/
-//  @javax.annotation.Nonnull
-//  @ApiModelProperty(required = true, value = "")
-//
-//  public Boolean getValid() {
-//    return valid;
-//  }
-//
-//
-//  public void setValid(Boolean valid) {
-//    this.valid = valid;
-//  }
-//
-//
   public CoordinatedUTCInquiry spare1(Integer spare1) {
-    
     this.spare1 = spare1;
     return this;
   }
@@ -258,10 +142,6 @@ public class CoordinatedUTCInquiry extends AisMessage {
             Objects.equals(getRepeatIndicator(), coordinatedUTCInquiry.getRepeatIndicator()) &&
             Objects.equals(getUserID(), coordinatedUTCInquiry.getUserID()) &&
             Objects.equals(getValid(), coordinatedUTCInquiry.getValid()) &&
-//    return Objects.equals(this.messageID, coordinatedUTCInquiry.messageID) &&
-//        Objects.equals(this.repeatIndicator, coordinatedUTCInquiry.repeatIndicator) &&
-//        Objects.equals(this.userID, coordinatedUTCInquiry.userID) &&
-//        Objects.equals(this.valid, coordinatedUTCInquiry.valid) &&
         Objects.equals(this.spare1, coordinatedUTCInquiry.spare1) &&
         Objects.equals(this.destinationID, coordinatedUTCInquiry.destinationID) &&
         Objects.equals(this.spare2, coordinatedUTCInquiry.spare2);
@@ -281,10 +161,6 @@ public class CoordinatedUTCInquiry extends AisMessage {
     sb.append("    repeatIndicator: ").append(toIndentedString(getRepeatIndicator())).append("\n");
     sb.append("    userID: ").append(toIndentedString(getUserID())).append("\n");
     sb.append("    valid: ").append(toIndentedString(getValid())).append("\n");
-//    sb.append("    messageID: ").append(toIndentedString(messageID)).append("\n");
-//    sb.append("    repeatIndicator: ").append(toIndentedString(repeatIndicator)).append("\n");
-//    sb.append("    userID: ").append(toIndentedString(userID)).append("\n");
-//    sb.append("    valid: ").append(toIndentedString(valid)).append("\n");
     sb.append("    spare1: ").append(toIndentedString(spare1)).append("\n");
     sb.append("    destinationID: ").append(toIndentedString(destinationID)).append("\n");
     sb.append("    spare2: ").append(toIndentedString(spare2)).append("\n");
@@ -402,7 +278,7 @@ public class CoordinatedUTCInquiry extends AisMessage {
 			// XXX spare
 			abm = abm.destinationID(jo.getInt(SERIALIZED_NAME_DESTINATION_I_D));
 			// XXX spare
-			System.out.println(abm);
+//			System.out.println(abm);
 		} catch (JSONException e) {
 //			logger.error("Error creating CoordinatedUTCInquiry", e);
 			System.out.println("Error creating CoordinatedUTCInquiry " + e);
